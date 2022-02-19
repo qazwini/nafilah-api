be583bbf0f377567e9c89c630f414ba825ce83cc
Mahdi Qazwini <mqazwini@umich.edu>
2022-02-19 14:05:31 -0500

    Add procfile

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const prayersRoutes = require('./src/controllers/prayers');
 
+require('dotenv').config()
+
 const app = express();
 app.use(express.json());
 
 // Set up routes
 app.use('/api/prayers/', prayersRoutes);
 
-app.get('/api/notes/', (request, response) => {
-    return response.json([{meow: 1}]);
+app.get('/api/meow/', (request, response) => {
+    return response.send("nya~");
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
